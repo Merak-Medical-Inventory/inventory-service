f337cbf199b85791841773d79fa90ddf09b7d64a
Rommel Liendo <cloudyrl1@gmail.com>
2020-08-09 22:16:08 -0400

    typeorm config class, ormconfig.json deleted

---
+++ src/config.ts
@@ -0,0 +1,26 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+import Brand from '@db/entity/Brand/Brand';
+import Category from '@db/entity/Category/Category';
+import Item from '@db/entity/Item/Item';
+import Presentation from '@db/entity/Presentation/Presentation';
+
+
+const typeOrmConfig: PostgresConnectionOptions = {
+    type: "postgres",
+    host: "merak-inventory.czdwqkwpxkdi.us-east-1.rds.amazonaws.com",
+    port: 5432,
+    username: "postgres",
+    password: "postgres",
+    database: "merak-inventory",
+    synchronize: false,
+    logging: false,
+    entities: [
+        Brand,
+        Category,
+        Item,
+        Presentation,
+    ]
+};
+
+export { typeOrmConfig };
