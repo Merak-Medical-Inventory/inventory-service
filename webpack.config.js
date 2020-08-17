a0bfe3df6d659bb55de9b2b8e0a8902c54d1528d
Rommel Liendo <cloudyrl1@gmail.com>
2020-08-16 20:54:02 -0400

    heroku configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
 // Using webpack 'cause ts build doesn't resolve alias paths: https://github.com/microsoft/TypeScript/issues/26722
-const path = require('path');
+const path = require("path");
 // this package handles all the external packages
-const nodeExternals = require('webpack-node-externals');
+const nodeExternals = require("webpack-node-externals");
 // help running shell commands with webpack before and after the build process
-const WebpackShellPlugin = require('webpack-shell-plugin');
+const WebpackShellPlugin = require("webpack-shell-plugin");
 // used to do the typechecking in a seperate process so the transpiling will be handled only by tsloader.
 // speed up compilation of code
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
-const {
-  NODE_ENV = 'production',
-} = process.env;
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+
+const { NODE_ENV = "production" } = process.env;
 
 module.exports = {
- // our entry server file
-  entry: './src/index.ts',
+  // our entry server file
+  entry: "./src/index.ts",
   // should be here so webpack knows that it handles node packages
-  target: 'node',
+  target: "node",
   // mode can be production or development
   mode: NODE_ENV,
   // enable watching only if it is development mode
-  watch: NODE_ENV === 'development',
+  watch: NODE_ENV === "development",
   externals: [nodeExternals()],
   // output path, i chose build but feel free to change it to anything
   // output file name [name]. means that it will create multiple code chunks for the build
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
   },
   // all file extensions to resolve, we might need to add file and images extensions if needed
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
     alias: {
       "@services": path.resolve(__dirname, "src/services/"),
       "@shared": path.resolve(__dirname, "src/shared/"),
@@ -40,15 +40,12 @@ module.exports = {
       "@helpers": path.resolve(__dirname, "src/helpers/"),
       "@migrations": path.resolve(__dirname, "src/migration/"),
       "@seeds": path.resolve(__dirname, "src/seeds/"),
-      "@middlewares": path.resolve(__dirname, "src/middlewares/")
-    } 
+      "@middlewares": path.resolve(__dirname, "src/middlewares/"),
+    },
   },
   plugins: [
+    new CaseSensitivePathsPlugin(),
     new ForkTsCheckerWebpackPlugin(),
-    new WebpackShellPlugin({
-     // when build ends run dev if the environment is development else run prod
-      onBuildEnd: (NODE_ENV === 'development') ? ['npm run run:dev'] : ['npm run run:prod']
-    })
   ],
   module: {
     rules: [
@@ -56,18 +53,21 @@ module.exports = {
         test: /\.ts$/,
         use: [
           {
-            loader: 'ts-loader',
+            loader: "ts-loader",
             options: {
-             // use tsconfig.prod only in development mode
-              configFile: NODE_ENV === 'development' ? 'tsconfig.json' : 'tsconfig.prod.json',
-              transpileOnly: true // and we use ForkTsCheckerWebpackPlugin for type checking
-            }
-          }
+              // use tsconfig.prod only in development mode
+              configFile:
+                NODE_ENV === "development"
+                  ? "tsconfig.json"
+                  : "tsconfig.prod.json",
+              transpileOnly: true, // and we use ForkTsCheckerWebpackPlugin for type checking
+            },
+          },
         ],
-      }
-    ]
+      },
+    ],
   },
   optimization: {
     minimize: false,
   },
-}
+};
