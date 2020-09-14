90b32b76e175533ab40e7728b108f64f87eea6f1
Rommel Liendo <cloudyrl1@gmail.com>
2020-09-13 22:20:28 -0400

    amount added to the Joi schema

--- src/shared/joi/Lot.ts
+++ src/shared/joi/Lot.ts
@@ -4,6 +4,7 @@ export const createLotsSchema = Joi.object().keys({
     order : Joi.number().required(),
     items : Joi.array().items(Joi.object().keys({
         id : Joi.number().required(),
-        dueDate : Joi.date().required()
+        dueDate : Joi.date().required(),
+        amount : Joi.number().required()
     }).required()).required()
 });
