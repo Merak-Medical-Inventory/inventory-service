53685180f9fbb1f512ce15b1a42da820545befd8
Rommel Liendo <cloudyrl1@gmail.com>
2020-09-13 22:15:47 -0400

    get lot by id and get all lots services created

--- src/controllers/Lot.ts
+++ src/controllers/Lot.ts
@@ -2,7 +2,32 @@ import { Request, Response , NextFunction } from 'express';
 import { handleSuccess } from '@helpers/succesHandler';
 import { ErrorHandler } from '@helpers/ErrorHandler/';
 import logger from '@shared/Logger';
-import { createLotsSvc } from '@services/lot';
+import { createLotsSvc, findLotSvc } from '@services/lot';
+
+
+export const getLotByIdCtrl = async (req : Request , res : Response , next : NextFunction) => {
+    try {
+        const { id } = req.params
+        const data = await findLotSvc({id})
+        if(!data) throw new ErrorHandler(404,"Lote no encontrado")
+        handleSuccess(200,"lote" , res,next,data);
+    } catch (e) {
+        logger.error("TCL: findLotByIdCtrl -> e", e)
+        next(e)
+    }
+}
+
+export const getAllLotsCtrl = async (req : Request , res : Response , next : NextFunction) => {
+    try {
+        const criteria = req.body
+        const data = await findLotSvc(criteria)
+        handleSuccess(200,"lotes" , res,next,data);
+    } catch (e) {
+        logger.error("TCL: findAllLotsCtrl -> e", e)
+        next(e)
+    }
+}
+
 
 export const createLotsCtrl = async (req : Request , res : Response , next : NextFunction) => {
     try {
--- src/routes/Lot.ts
+++ src/routes/Lot.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 import { joiValidator } from '@middlewares/joi';
 import { sessionCheck } from '@middlewares/auth/auth';
 import { createLotsSchema } from '@shared/joi/Lot';
-import { createLotsCtrl } from '@controllers/Lot';
+import { createLotsCtrl, getAllLotsCtrl, getLotByIdCtrl } from '@controllers/Lot';
 
 const router = Router();
 
+router.get('/',[sessionCheck],getAllLotsCtrl);
+router.get('/:id',[sessionCheck],getLotByIdCtrl);
 router.post('/',[sessionCheck,joiValidator(createLotsSchema)],createLotsCtrl);
 
 export default router;
