e17885c4d674545f3faf50a156a2050f9b46e4f7
Rommel Liendo <cloudyrl1@gmail.com>
2021-03-20 19:25:46 -0400

    feat(BLOCKCHAIN instance)

---
+++ src/helpers/transaction/index.ts
@@ -0,0 +1,46 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+
+const blockchainAxiosConfig: AxiosRequestConfig = {
+  baseURL: process.env.BLOCKCHAIN_HOST,
+  timeout: 10000,
+  timeoutErrorMessage: "Blockchain request time exceeded",
+  headers: {
+    "x-api-token": `${process.env.API_TOKEN}`,
+  },
+};
+
+const blockchainInstance: AxiosInstance = axios.create(blockchainAxiosConfig);
+
+export const createTransaction = async (
+  sender: string,
+  receiver: string,
+  inventory1: string,
+  inventory2: string,
+  item: string,
+  amount: string,
+  type: string
+) => {
+  try {
+    const response = await blockchainInstance.post("/api/transaction", {
+      sender,
+      receiver,
+      inventory1,
+      inventory2,
+      item,
+      amount,
+      type,
+    });
+    return response.data;
+  } catch (e) {
+    throw e;
+  }
+};
+
+export const getTransaction = async (id: string) => {
+  try {
+    const response = await blockchainInstance.get(`/api/transaction/${id}`);
+    return response.data;
+  } catch (e) {
+    throw e;
+  }
+};
