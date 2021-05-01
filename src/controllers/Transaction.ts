2d38218d9d69e17cdf801a5f845a85c1de643606
gabrielbaron16 <gabrielbu161@gmail.com>
2021-04-30 23:16:16 -0400

    Se Muestran las Transacciones para un Inventario en Específico

--- src/controllers/Transaction.ts
+++ src/controllers/Transaction.ts
@@ -1,6 +1,6 @@
 import { Request, Response , NextFunction } from 'express';
 import { handleSuccess } from '@helpers/succesHandler';
-import { findAllTransactionsSvc } from '@services/transaction';
+import {findAllTransactionsSvc, findInventoryTransactionsSvc} from '@services/transaction';
 import logger from '@shared/Logger';
 import { getBcTransactionSvc } from '@helpers/transaction';
 
@@ -18,9 +18,21 @@ export const findAllTransactionCtrl = async (req: IRequest, res: Response, next:
     }
 };
 
+export const findInventoryTransactionsCtrl = async (req: IRequest, res: Response, next: NextFunction) => {
+    try {
+        const {id} = req.params;
+        console.log(id);
+        const data = await findInventoryTransactionsSvc(Number(id));
+        handleSuccess(200, 'Información de las transacciones', res, next, data);
+    } catch (e) {
+        logger.error('ERROR: controller -> findAllTransactionCtrl', e);
+        next(e);
+    }
+};
+
 export const findBcTransactionCtrl = async (req: IRequest, res: Response, next: NextFunction) => {
     try {
-        const {id} = req.params
+        const {id} = req.params;
         const data = await getBcTransactionSvc(id);
         handleSuccess(200, 'Información de las transacciones', res, next, data.data);
     } catch (e) {
